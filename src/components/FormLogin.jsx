22867bd5405de5c651d684f781c95a056f91cfb6
Willy Prado <pradopublicity@gmail.com>
2021-09-25 18:38:18 -0500

    Agregando validación de campos de ingreso

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,37 +1,61 @@
-import * as React from "react";
+import { useState, useRef } from "react";
 // import Box from "@mui/material/Box";
 // import TextField from "@mui/material/TextField";
 // import { makeStyles } from "@material-ui/styles";
 import { Button, TextField, Box, FormControl } from "@mui/material";
 
 export default function FormPropsTextFields() {
+  const [emailValido, setEmailValido] = useState(true);
+  const [passValido, setPassValido] = useState(true);
+  const emailRef = useRef();
+  const passRef = useRef();
+
   const handleSubmit = e => {
-    console.log(e);
     e.preventDefault();
+    const email = emailRef.current.value;
+    const pass = passRef.current.value;
+
+    if (email === "") {
+      setEmailValido(false);
+    }
+    if (pass === "") {
+      setPassValido(false);
+    }
+
+    console.log(emailRef.current.value);
+    console.log(passRef.current.value);
   };
+
   return (
     <Box
       component="form"
+      onSubmit={handleSubmit}
       sx={{
         "& .MuiTextField-root": { m: 1, width: "50ch" },
       }}
       noValidate
       autoComplete="off"
     >
-      <FormControl onClick={handleSubmit}>
+      <FormControl>
         <TextField
           required
+          inputRef={emailRef}
+          error={!emailValido}
+          onChange={() => setEmailValido(true)}
           id="outlined-required"
           label="Correo"
           type="email"
-          defaultValue="correo@correo.com"
+          helperText={!emailValido ? "Ingresa un correo valido" : ""}
         />
         <TextField
           required
+          inputRef={passRef}
+          error={!passValido}
+          onChange={() => setPassValido(true)}
           id="outlined-password-input"
           label="Contraseña"
           type="password"
-          autoComplete="current-password"
+          helperText={!passValido ? "Ingresa contraseña" : ""}
         />
         <Button type="submit">Ingresar</Button>
       </FormControl>
